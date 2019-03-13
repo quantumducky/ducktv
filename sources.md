32547eec4e16e5e9da749378d98e9fe6abf6ce48
quantumducky <quantumducky@gmail.com>
2019-03-14 01:11:47 +0200

    testing

--- sources.md
+++ sources.md
@@ -32,3 +32,4 @@ ottv.tk#.XML
 Test:
 https://vs11.cdnlast.com/s/9c3ebac03c3c869c495d42b8784bca8f/Terra.Nova/s01e01-02_720.mp4
 https://vs11.cdnlast.com/s/9c3ebac03c3c869c495d42b8784bca8f/Cobra.Kai-AlexFilm/s01e01_720.mp4
+
--- stuff/http.js
+++ stuff/http.js
@@ -1,33 +1,15 @@
-var querystring = require('querystring');
-var request = require('request');
+let P = require('bluebird'),
+request = P.promisifyAll(require("request")),
+Iconv = require('iconv-lite');
 
 
-// var form = {
-//     q: 'XGY//mFLNTnMI/9aSn6dOUux+PzRfkPQ47A6+P7WFRc+FBATNeoW7BSICPnun8NrU0dnd6qa8ToAKqJAZ0xn+eJSjQe6hCmZ06JQc+Quj8n0P+Lne9KASvPX40cKLJS1Irfak+cJTfdpJEPkwEDiSUsLMjSP284mfzRtuCo808pvg+k4NKZC1Va6WibVM1zd',
-//     ref: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyZWZfaG9zdCI6ImhkcmV6a2EuYWciLCJyZXFfaG9zdCI6InMxLmNkbmFwcG9ubGluZS5jb20iLCJleHAiOjE1NTIzODY0MjQsInRva2VuIjoiM2IxZmRkMWU1NGI5YTVlZGM2NzdmNWFiNjU5Y2JlZjUifQ._zxFnyr8pxh514BHLMmos1qMxYoSnq8T14gVEfF-o2k'
-// };
+let URL_STRING = 'https://www.google.com/search?q=%D0%9B%D1%83%D1%87%D1%88%D0%B5+%D0%B7%D0%B2%D0%BE%D0%BD%D0%B8%D1%82%D0%B5+%D0%A1%D0%BE%D0%BB%D1%83&hl=en&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjy5Pf-koDhAhUtx4sKHTY7DQQQ_AUIDigB&biw=1280&bih=607'
 
-// var formData = querystring.stringify(form);
-// var contentLength = formData.length;
 
-// request({
-//   headers: {
-//     'Content-Length': contentLength,
-//     'Host': 'mastarti.com',
-//     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0',
-//     'Accept': 'application/json, text/javascript, */*; q=0.01',
-//     'Accept-Language': 'en-US,en;q=0.5',
-//     // 'Accept-Encoding': 'gzip, deflate',
-//     'Referer': 'http://mastarti.com/serial/3b1fdd1e54b9a5edc677f5ab659cbef5/iframe?episode=10&nocontrols=1&pid_b2cb5260=6c6cba9e&ref=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyZWZfaG9zdCI6ImhkcmV6a2EuYWciLCJyZXFfaG9zdCI6InMxLmNkbmFwcG9ubGluZS5jb20iLCJleHAiOjE1NTIzODY0MjQsInRva2VuIjoiM2IxZmRkMWU1NGI5YTVlZGM2NzdmNWFiNjU5Y2JlZjUifQ._zxFnyr8pxh514BHLMmos1qMxYoSnq8T14gVEfF-o2k&season=3',
-//     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-//     'X-Requested-With': 'XMLHttpRequest',
-//     'DNT': '1',
-//     'Connection': 'keep-alive'
-//   },
-//   uri: 'http://mastarti.com/vs',
-//   body: formData,
-//   method: 'POST'
-// }, function (err, res, body) {
-//   console.log(res);
-//   console.log(body);
-// });
+
+request(URL_STRING, function (error, response, body) {
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode); 
+  body = Iconv.decode(new Buffer(body, 'binary'), 'win1251');
+  console.log('body:', body);
+});
