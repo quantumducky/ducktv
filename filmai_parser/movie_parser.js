98ac9276dbb90ee09d2dd5c9b5fdcb5c57ebc2a2
quantumducky <quantumducky@gmail.com>
2019-10-31 12:17:28 +0200

    testing

--- filmai_parser/movie_parser.js
+++ filmai_parser/movie_parser.js
@@ -7,7 +7,8 @@ const getUrls = require('get-urls');
 const BASE_URL = 'https://www.filmai.org';
 const LIMIT = 100;
 
-const URL = 'https://www.filmai.org/9223-savas-zmogus-labiausiai-ieskomas-2019-inside-man-most-wanted.html';
+// const URL = 'https://www.filmai.org/9223-savas-zmogus-labiausiai-ieskomas-2019-inside-man-most-wanted.html';
+const URL = 'https://www.filmai.org/236-septyneri-metai-tibete-seven-years-in-tibet-1997.html';
 
 
 // main();
