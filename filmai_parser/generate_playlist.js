010e5ca5f61b501411ac68d22ff2a159c3a7ead0
quantumducky <quantumducky@gmail.com>
2019-10-31 17:41:06 +0200

    testing

--- filmai_parser/generate_playlist.js
+++ filmai_parser/generate_playlist.js
@@ -11,7 +11,7 @@ async function playlistByGenres() {
   let movies = moviesLog.movies;
 
   let results = `<?xml version="1.0" encoding="UTF-8"?>\n<items>`;
-  movies.slice(255,280).forEach(movie => {
+  movies.slice(10,90).forEach(movie => {
     results += createMovieItem(movie);
   })
   results += '\n</items>';
@@ -47,13 +47,13 @@ function createMovieItem(movie) {
           </center>
           <div style="font-size: 1.4em;">
             <div>
-              <span style="color: #ffaf59;"><b>Šalis: </b></span><span style="color: #C0C0C0">${movie.country}</span>
+              <span style="color: #ffaf59;"><b>Šalis: </b></span><span style="color: #C0C0C0">${movie.country ? movie.country : '-'}</span>
             </div>
             <div>
-              <span style="color: #ffaf59;"><b>Aktoriai: </b></span><span style="color: #C0C0C0">${movie.actors}</span>
+              <span style="color: #ffaf59;"><b>Aktoriai: </b></span><span style="color: #C0C0C0">${movie.actors ? movie.actors : '-'}</span>
             </div>
             <div>
-              <span style="color: #ffaf59;"><b>Režisierius: </b></span><span style="color: #C0C0C0">${movie.director}</span>
+              <span style="color: #ffaf59;"><b>Režisierius: </b></span><span style="color: #C0C0C0">${movie.director ? movie.director : '-'}</span>
             </div>
           </div>
         </div>
@@ -63,19 +63,19 @@ function createMovieItem(movie) {
             <div style="color: #ffaf59; font-size: 1.6em; margin-bottom:8px;">${movie.title}</div>
           </center>
           <div>
-            <img src="https://raw.githubusercontent.com/quantumducky/ducktv/master/img/imdb_124.png" width="16px"></img>&nbsp;<span style="color: #C0C0C0">${movie.imdbText ? movie.imdbText : '-'}</span>
+            <img src="https://raw.githubusercontent.com/quantumducky/ducktv/master/img/imdb_32.png" width="20px"></img>&nbsp;<span style="color: #C0C0C0">IMDB: ${movie.imdbText ? movie.imdbText.slice(6) : '-'}</span>
           </div>
           <div>
-            <span style="color: #ffaf59;"><b>Kalba: </b></span><span style="color: #C0C0C0">${movie.language}</span>
+            <span style="color: #ffaf59;"><b>Kalba: </b></span><span style="color: #C0C0C0">${movie.language ? movie.language : '-'}</span>
           </div>
           <div>
-            <span style="color: #ffaf59;"><b>Metai: </b></span><span style="color: #C0C0C0">${movie.year}</span>
+            <span style="color: #ffaf59;"><b>Metai: </b></span><span style="color: #C0C0C0">${movie.year ? movie.year : '-'}</span>
           </div>
           <div>
             <span style="color: #ffaf59;"><b>Žanrai: </b></span><span style="color: #C0C0C0">${movie.genres ? movie.genres.join(', ') : '-'}</span>
           </div>
           <div>
-            <span style="color: #ffaf59;"><b>Trukmė: </b></span><span style="color: #C0C0C0">${movie.length}</span>
+            <span style="color: #ffaf59;"><b>Trukmė: </b></span><span style="color: #C0C0C0">${movie.length ? movie.length : '-'}</span>
           </div>
           <div>
             <p>
