7efe377843b061e0133284dccf95afc2a63c51dd
quantumducky <quantumducky@gmail.com>
2020-12-12 19:11:29 +0200

    testing

--- sources.md
+++ sources.md
@@ -16,18 +16,12 @@ http://ottv.tk/public/plst/plstfb/playlist.php
 http://1korol.at.ua/
 http://tvpremiumhd.club/
 
+http://hdtv.pp.ua/?code=40
+https://pastebin.ulvis.net/rSwTQWP9
 
-TV1
-http://e01.plius.tv:64144/n134/index.m3u8
-
-BTV
-http://e01.plius.tv:64144/n133/index.m3u8
-
-Info TV
-http://e01.plius.tv:64144/n135/index.m3u8
-
-TV Polonia
-http://e02.plius.tv:64144/n38/index.m3u8
+Apps:
+https://homeiptv.com/
+https://www.duplexiptv.com/index.html
 
 
 API:
@@ -41,19 +35,7 @@ http://ottv.tk/public/plst/plstfb/playlist.php?ott
 ottv.tk#.XML
 
 
-Dr Q:
-https://serialage.tv
-http://onser.info
 
-
-
-Movie sources working lengths:
-filmix.co:
-https://nl3.cdnlast.com/s/f29b988d7c466bcdfbb273c07fc95f4b/uhd_mc/Knives.Out.2019.BDRip.2160p_720.mp4
-
-
-https://github.com/valverde-marcelo/kodi2webos
-https://github.com/raphamorim/react-tv
-
-sanity.io
+linguistics LT
+https://benjamins.com/catalog/cilt.254
 
--- test.js
+++ test.js
@@ -1,62 +1,26 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
-const INTERVAL =  3;
-const ROOT_URL = 'http://www.music.lt/';
-const MAIN_URL = 'http://www.music.lt/Lietuva-grupes-1.html';
+const INTERVAL =  5;
 let i = 0;
 
 
 (async () => {
-  let response = await fetch(MAIN_URL);
-  let res = await response.text();
-  let $ = cheerio.load(res);
-
-  let groupList = [];
-  $('div.content tbody tr td a').each((i, elem) => {
-    if ($(elem).attr('href').startsWith('lt')) {
-      groupList.push({name: $(elem).text(), url: ROOT_URL + $(elem).attr('href')})
-    }
-  });
-
-  groupList.forEach(item => {
-    let response = await fetch(item.url);
-    let res = await response.text();
-    let $ = cheerio.load(res);
-  })
-
-  while (fetched < LIMIT && tried < urls.length) {
-    let movieUrl = urls[tried++];
-    if (fetchedSet.has(movieUrl) || failedSet.has(movieUrl)) {
-      continue;
-    }
-    await parseMovie(movieUrl)
-      .then(movie => {
-        movies.push(movie);
-        fetchedSet.add(movieUrl);
-        console.log(`Fetched movie (${fetched+1}): ${movieUrl}`);
-        fetched++;
-      })
-      .catch(() => {
-        console.log(`Failed to fetch movie (${fetched+1}): ${movieUrl}`);
-        failedSet.add(movieUrl);
-      });
-    await new Promise((resolve) => setTimeout(resolve, WAIT_TIME));
-  }
-
   // mainInterval = setInterval(fetchUrls, INTERVAL);
+  await fetchUrls();
+  console.log('done');
 })();
 
 async function fetchUrls() {
-  if (i === 10000) {
-    clearInterval(mainInterval);
-  }
+  // if (i === 10000) {
+  //   clearInterval(mainInterval);
+  // }
 
-  const url = `https://susivienijimas.lt/`;
+  const url = `http://e01.plius.tv:64144/n128/index.m3u8`;
 
   await fetch(url).then(res => {
-    console.log(`Call [${i}]: ${res.status}`);
-  });
+    return res.text();
+  }).then(data => console.log(data));
 
-  i++;
+  // i++;
 }
